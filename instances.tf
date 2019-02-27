36a9e618c25c146b3be00694f0b1cba9be2f54fb
Weliff Lima <weliff.14@gmail.com>
2019-02-27 16:45:02 -0300

    Adiciona configurações do cluster Swarm e Portainer

--- instances.tf
+++ instances.tf
@@ -13,7 +13,10 @@ resource "aws_instance" "instances" {
 
   key_name = "${aws_key_pair.keypair.key_name}"
 
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}", 
+    "${aws_security_group.allow_outbound.id}", 
+    "${aws_security_group.cluster_communication.id}",
+    "${aws_security_group.allow_portainer.id}"]
 
   tags {
     Name = "hibicode_instances"
@@ -24,6 +27,7 @@ data "template_file" "hosts" {
   template = "${file("./templates/hosts.tpl")}"
 
   vars {
+    PRIVATE_IP_0 = "${aws_instance.instances.*.private_ip[0]}"
     PUBLIC_IP_0 = "${aws_instance.instances.*.public_ip[0]}"
     PUBLIC_IP_1 = "${aws_instance.instances.*.public_ip[1]}"
     PUBLIC_IP_2 = "${aws_instance.instances.*.public_ip[2]}"
--- security.tf
+++ security.tf
@@ -22,3 +22,64 @@ resource "aws_security_group" "database" {
     }
 }
 
+resource "aws_security_group" "allow_outbound" {
+    vpc_id = "${aws_vpc.main.id}"
+    name = "hibicode_allow_outbound"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "cluster_communication" {
+    vpc_id = "${aws_vpc.main.id}"
+    name = "hibicode_cluster_communication"
+
+    ingress {
+        from_port = 2377
+        to_port = 2377
+        protocol = "tcp"
+        self = true
+    }
+
+    ingress {
+        from_port = 7946
+        to_port = 7946
+        protocol = "tcp"
+        self = true
+    }
+
+
+    ingress {
+        from_port = 7946
+        to_port = 7946
+        protocol = "udp"
+        self = true
+    }
+
+
+    ingress {
+        from_port = 4789
+        to_port = 4789
+        protocol = "udp"
+        self = true
+    }
+}
+
+resource "aws_security_group" "allow_portainer" {
+    vpc_id = "${aws_vpc.main.id}"
+    name = "hibicode_allow_portainer"
+
+    ingress {
+        from_port = 9000
+        to_port = 9000
+        protocol = "tcp"
+        cidr_blocks = ["${var.my_public_ip}"]
+    }
+  
+}
+
+
